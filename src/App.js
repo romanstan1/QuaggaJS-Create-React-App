93861f199b1e89e982748d11b4bf99a361ed3332
Roman Stankiewicz <roman.stan3@gmail.com>
2018-04-16 11:44:11 +0100

    updated readme

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Scanner from './Scanner';
 import Result from './Result';
 
-
 class App extends Component {
 
   state = {
@@ -11,7 +10,6 @@ class App extends Component {
   }
 
   _scan = () => {
-    console.log("this",this)
     this.setState({scanning: !this.state.scanning});
   }
 
@@ -24,7 +22,7 @@ class App extends Component {
       <div>
           <button onClick={this._scan}>{this.state.scanning ? 'Stop' : 'Start'}</button>
           <ul className="results">
-            {this.state.results.map((result) => (<Result key={result.codeResult.code} result={result} />))}
+            {this.state.results.map((result, i) => (<Result key={result.codeResult.code + i} result={result} />))}
           </ul>
           {this.state.scanning ? <Scanner onDetected={this._onDetected}/> : null}
       </div>
--- src/Scanner.js
+++ src/Scanner.js
@@ -12,14 +12,14 @@ class Scanner extends Component {
             constraints: {
                 width: 640,
                 height: 480,
-                facing: "environment" // or user
+                facingMode: "environment" // or user
             }
         },
         locator: {
             patchSize: "medium",
             halfSample: true
         },
-        numOfWorkers: 2,
+        numOfWorkers: 4,
         decoder: {
             readers : [ "code_128_reader"]
         },
